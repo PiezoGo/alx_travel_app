8d547e53cab6214571abf8a8a0a441431f32dc9d
PiezoGo <piezo5go@gmail.com>
2025-07-28 16:55:00 +0300

    finale

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'listing',
+    'listings',
     'rest_framework',
     'corsheaders',
     'drf_yasg',
--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -33,7 +33,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('listing.urls')),
+    path('', include('listings.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
